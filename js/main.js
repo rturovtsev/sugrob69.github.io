63b187657040fd3849e15db4c2e08b56739a9b2c
SuGROB <sugrob.boid@gmail.com>
2014-07-28 17:44:02 +0400

    commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ hljs.initHighlightingOnLoad();
 $( document ).ready(function() {
     $('.content').load('jquery.html #t_menu');
 
+    $('li', '.nav').on('click', function(){
+        var link = $(this).attr('data-src');
+        $('.content').load(link + ' #t_menu');
+    });
+
     $('#content').on('click', '#t_menu li', function(){
         var link = $(this).attr('class');
         var tem, temlink;
@@ -10,6 +15,15 @@ $( document ).ready(function() {
         switch (link.charAt(0)) {
             case 'j':
                 tem = "jquery.html";
+                break;
+            case 'js':
+                tem = "js.html";
+                break;
+            case 'html':
+                tem = "html.html";
+                break;
+            case 'css':
+                tem = "css.html";
                 break;
         }
 
