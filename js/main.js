59fecf2ff9e6fc04e2143aa34920c7917c62fc25
SuGROB <sugrob.boid@gmail.com>
2014-07-28 17:08:49 +0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,18 +1,5 @@
 hljs.initHighlightingOnLoad();
 
 $( document ).ready(function() {
-    $.ajax({
-        url: 'jquery.html',             // указываем URL
-        dataType : "html",
-        success: function (data) { // вешаем свой обработчик на функцию success
-            if (typeof(data) == 'undefined' || !data) {
-                // произошла ошибка
-                alert('Произошла ошибка.');
-            } else {
-                var menu = $.html(data);
-                var m = menu.find('#t_menu').html();
-                $('.content').append(m);
-            }
-        }
-    });
+    $('.content').load('jquery.html #t_menu');
 });
