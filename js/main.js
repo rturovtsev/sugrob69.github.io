ee8172fce7c90d48d282944d4801783ce0213670
SuGROB <sugrob.boid@gmail.com>
2014-07-28 17:21:48 +0400

    commit

--- js/main.js
+++ js/main.js
@@ -2,4 +2,17 @@ hljs.initHighlightingOnLoad();
 
 $( document ).ready(function() {
     $('.content').load('jquery.html #t_menu');
+
+    $('#content').on('click', '#t_menu li', function(){
+        var link = $(this).attr('class');
+        var tem;
+
+        switch (link.charAt(0)) {
+            case 'j':
+                tem = "jquery.html";
+                break;
+        }
+
+        $('.content').load(tem link);
+    });
 });
