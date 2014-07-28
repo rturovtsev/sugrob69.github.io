f72b870bce2ff7ca723908af571f1d0f24a20350
SuGROB <sugrob.boid@gmail.com>
2014-07-28 16:40:48 +0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 hljs.initHighlightingOnLoad();
+
+$( document ).ready(function() {
+    $.ajax({
+        url: 'jquery.html #t_menu',             // указываем URL
+        dataType : "html",
+        success: function (data) { // вешаем свой обработчик на функцию success
+            if (typeof(data) == 'undefined' || !data) {
+                // произошла ошибка
+                alert('Произошла ошибка.');
+            } else {
+                $('.content').append(data);
+            }
+        }
+    });
+});
