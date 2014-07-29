e5f50b76621c68805a26699d5bd87772e92252b5
SuGROB <sugrob.boid@gmail.com>
2014-07-29 12:55:47 +0400

    commit

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 hljs.initHighlightingOnLoad();
 
 $( document ).ready(function() {
-    $('.content').load('jquery.html #t_menu');
+    //загружаем первоначальное меню от jquery
+    $('.content').load('jquery.html #t_menu');  
 
-    $('li', '.nav').on('click', function(){
+
+    //подкружаем меню при клике в главном меню
+    $('li', '.nav').on('click', function(){  
+        $(this)
+            .addClass('active')
+            .siblings()
+            .removeClass('active');
+
         var link = $(this).attr('data-src');
-        $('.content').load(link + ' #t_menu');
+        $('.content').load(link + ' #t_menu', function(){
+            var name = $('li:first' , '#t_menu').text();
+            $('a', '.breadcrumbs').text(name);
+        });
     });
 
+
+    //подгрузка контента при клике в дополнительном меню
     $('#content').on('click', '#t_menu li', function(){
         var link = $(this).attr('class');
         var name = $(this).text();
@@ -36,6 +49,7 @@ $( document ).ready(function() {
 
         temlink = tem + " #" + link;
 
+        //загрузка и подсветка контента
         $('.content').load(temlink, function(){
             $('pre').each(function(i, block) {
                 hljs.highlightBlock(block);
@@ -45,6 +59,8 @@ $( document ).ready(function() {
                 hljs.highlightBlock(block);
             });
         });
+
+        //обновляем заголовок
         $('a', '.breadcrumbs').text(name);
     });
 });
